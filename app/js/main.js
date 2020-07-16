5b8d145fd99218b574bc84f46c7d9500288cedbd
Artur Kudaev <artur.kudaev@ria.com>
2020-07-16 23:08:08 +0300

    update config

--- app/js/main.js
+++ app/js/main.js
@@ -1,27 +1,5 @@
-window.onload = function showMenu() {
-	let btn = document.getElementById('menu-btn');
-	btn.onclick = function () {
-		var menu = document.querySelectorAll('.main-menu ul')[0];
-		menu.classList.toggle("hide");
-	};
-};
+const test = 1;
+const str = `Для теста - ${test}`;
 
-function dev() {
-	let place = document.querySelectorAll('footer')[0];
-	let a = document.createElement('a');
-	a.innerHTML = 'Разработчик сайта';
-	a.setAttribute('href', 'http://webinme.ru/');
-	a.setAttribute('target', '_blank');
-	a.setAttribute('rel', 'nofollow');
-	a.style.cssText = "color: #777;\
-	display: block;\
-	margin: 25px auto 20px;\
-	text-align: center;\
-	text-decoration: underline;\
-	text-transform: uppercase;\
-	font-size: 12px;\
-	";
-	place.appendChild(a);
-};
+console.log(str);
 
-dev();
--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
 var del = require('del');
-var autoprefixer = require('gulp-autoprefixer');
-var autoprefixBrowsers = ['> 1%', 'last 10 versions', 'firefox >= 4', 'safari 7', 'safari 8', 'IE 8', 'IE 9', 'IE 10', 'IE 11'];
-var tinypng = require('gulp-tinypng');
-var jsmin = require('gulp-jsmin');
-var cssmin = require('gulp-cssmin');
+var autoprefixer = require('autoprefixer');
+var cssnano = require('cssnano');
 var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var runSequence = require('run-sequence');
+var babel = require('gulp-babel');
 
 sass.compiler = require('node-sass');
  
@@ -17,59 +17,62 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('app/css'));
 });
 
-gulp.task('minify-css', () => {
-  return gulp.src('app/css/**/*.css')
-    .pipe(cssmin())
-    .pipe(gulp.dest('dist/css'));
+
+gulp.task('css', function () {
+	var plugins = [
+		autoprefixer({overrideBrowserslist: ['last 4 versions', 'Chrome >= 41', 'iOS >= 7', 'Safari >= 7']}),
+		cssnano()
+	];
+	return gulp.src('./app/css/**/*.css')
+			.pipe(postcss(plugins))
+			.pipe(gulp.dest('./dist/css'));
 });
 
-gulp.task('autoprefixer', function() {
-  return gulp.src('app/css/*css')
-    .pipe(autoprefixer())
-    .pipe(gulp.dest('dist/css'));
+gulp.task('js', function (){
+	return gulp.src('./app/js/**/*.js')
+			.pipe(babel())
+			.pipe(gulp.dest('./dist/js'));
 });
 
-gulp.task('minify-js', function() {
-  return gulp.src('app/js/**/*js')
-  	.pipe(jsmin())
-    .pipe(gulp.dest('dist/js'));
-});
 
 gulp.task('browser-sync', function() {
 	browserSync({
 		server: {
-			baseDir: 'app'
+			baseDir: './app'
 		},
 		notify: false
 	})
 });
 
-gulp.task('tinypng', function() {
-	return gulp.src('app/img/**/*')
-	.pipe(tinypng('u8rfBPVHsWVKcYXUJEo0v1Y7ZQklnmE0'))
-	.pipe(gulp.dest('dist/img'))
-});
-
 gulp.task('del', function() {
-	return del.sync('dist');
+	return del.sync('/.dist');
 });
 
-gulp.task('build', ['del', 'sass', 'minify-css', 'autoprefixer', 'minify-js', 'tinypng'], function() {
-	var buildHTML = gulp.src('app/**/*.html')
-	.pipe(gulp.dest('dist/'))
+gulp.task('build', function(callback) {
+	runSequence('del',
+			'sass',
+			'css',
+			'js',
+			callback);
 
-	var buildAll = gulp.src('app/**/*.php')
-	.pipe(gulp.dest('dist'))
+	gulp.src('./app/**/*.html')
+			.pipe(gulp.dest('./dist'));
 
-	var favicon = gulp.src('app/favicon.ico')
-	.pipe(gulp.dest('dist'))
+	gulp.src('./app/**/*.php')
+			.pipe(gulp.dest('./dist'));
 
-	var buildFonts = gulp.src('app/fonts/**/*')
-	.pipe(gulp.dest('dist/fonts'))
+	gulp.src('./app/favicon.ico')
+			.pipe(gulp.dest('./dist'));
+
+	gulp.src('./app/fonts/**/*')
+			.pipe(gulp.dest('./dist/fonts'));
+
+	gulp.src('./app/img/**/*')
+			.pipe(gulp.dest('./dist/img'));
 });
 
 gulp.task('watch', ['sass', 'browser-sync'], function(){
-	gulp.watch('app/sass/*.sass', ['sass']);
-	gulp.watch('app/**/*.html', browserSync.reload);
-	gulp.watch('app/**/*.js', browserSync.reload);
+	gulp.watch('./app/sass/*.sass', ['sass']);
+	gulp.watch('./app/**/*.html', browserSync.reload);
+	gulp.watch('./app/**/*.js', browserSync.reload);
 });
